c2242b203ac216c9caf3108d46530a82faaab542
mamba0328 <valentindjejylej@gmail.com>
2021-12-23 22:08:52 +0200

    add a book functionality has been added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-//add a book to the list 
-const list = document.getElementById('list')
-const addBook = document.querySelector('#add')
-
-addBook.addEventListener('click', () => {
-const div = document.createElement('div')
-list.appendChild(div)
-})
-
-//creats a book with a entered  properties and stores it in the array
+//constructor and array in which new books will be stored 
 const booksList = [];
 
 const book = function (name, author, year, pages, description) { 
@@ -17,4 +8,33 @@ const book = function (name, author, year, pages, description) {
     this.pages = pages; 
     this.description = description; 
 }
+
+const Dune = new book ("Dune","Dune","Dune","Dune","Dune"); 
+booksList.push(Dune);
+
+//add a book to the list 
+const list = document.getElementById('list');
+const pattern = document.querySelector('.book')//needed to display new books
+const newBook = document.getElementById('newBook')//button
+const nameInput = document.getElementById('Name');
+const authorInput = document.getElementById('Author');
+const descriptionInput = document.getElementById('Description');
+const yearInput = document.getElementById('Year');
+const pagesInput = document.getElementById('Pages');
+
+newBook.addEventListener('click', () => { 
+    const theBook = new book(nameInput.value, authorInput.value, yearInput.value, pagesInput.value, descriptionInput.value)
+    console.log(theBook)
+})
+
+//add a new book window pops up
+const addBook = document.querySelector('#add')
+const creator = document.getElementById('creator')
+addBook.addEventListener('click', () => {
+    if (creator.style.display === 'flex') {
+        creator.style.display = 'none'
+     } else creator.style.display = 'flex'
+    
+})
+
 
